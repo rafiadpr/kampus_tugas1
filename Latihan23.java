d62e22d44b284bb8b635007cb7216254166a76c2
rafiadpr <work.rafiadrianp@gmail.com>
2024-09-03 07:51:39 +0700

    first commit

---
+++ Latihan23.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan23
+ */
+public class Latihan23 {
+
+    public static void main(String[] args) {
+        System.out.print("Nama Saya Andre");
+        // System.out.print("\nSaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
---
+++ Tugas1_23.java
@@ -0,0 +1,15 @@
+public class Tugas1_23 {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Rafi Adrian Prasetya");
+        System.out.println("NIM     : 244107020026");
+        System.out.println("Kelas   : 1F");
+        System.out.println("No Absen: 23");
+        System.out.println("Prodi   : Teknik Informatika");
+        System.out.println("Alamat  : Jalan Danau Sentani Utara");
+        System.out.println("No HP   : 081328878542");
+        System.out.println("Hobi    : Main Game");
+    }
+}
